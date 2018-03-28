761cd3ba845b97e3f75d6a035a15968cf4a6d49e
Kean <chankeanyeow@gmail.com>
2018-03-29 03:13:27 +0800

    v2.0

--- app.js
+++ app.js
@@ -1,81 +1,95 @@
 //funtion
 
-function addition(){
-		
-		var ourAnswer = document.getElementById("answer").value;
-				
-		if (ourAnswer==compAnswer){			
-			score++;
-			document.getElementById("output").innerHTML = score;
-			newCard();
-		}else {
-			score--;
-			document.getElementById("output").innerHTML = score;
-			newCard();
-}
+function scoring() {
+
+	var ourAnswer = document.getElementById("answer").value;
+
+	if (ourAnswer == compAnswer) {
+		score++;
+		document.getElementById("output").innerHTML = score;
+		newCard();
+	} else {
+		score--;
+		document.getElementById("output").innerHTML = score;
+		newCard();
+	}
 }
 
 
-function newCard(){
-	
+function newCard() {
+
+	/*	document.getElementById("answer").value = '';
+		num1 = Math.floor(Math.random()*10)+1;
+		num2 = Math.floor(Math.random()*10)+1;
+		document.getElementById("header").innerHTML= num1 +"+" +num2;
+		compAnswer = num1+num2;
+	*/
 	document.getElementById("answer").value = '';
-	num1 = Math.floor(Math.random()*10)+1;
-	num2 = Math.floor(Math.random()*10)+1;
-	document.getElementById("header").innerHTML= num1 +"+" +num2;
-	compAnswer = num1+num2;
-	SetFocus()
-	
-/*	ranOpr = Math.floor(Math.random()*4)+1;
-	switch (compAnswer)
-	case 1
-	Addiditon:
-	case 2 
-	Subtract;
-	break;
-	case 3
-	case 4*/
-	
-	
-	
+
+	switch (Math.floor(Math.random() * 4) + 1) {
+		case 1:
+			num1 = Math.floor(Math.random() * 10) + 1;
+			num2 = Math.floor(Math.random() * 10) + 1;
+			document.getElementById("header").innerHTML = num1 + "+" + num2;
+			compAnswer = num1 + num2;
+			break;
+		case 2:
+			num1 = Math.floor(Math.random() * 10) + 1;
+			num2 = Math.floor(Math.random() * 10) + 1;
+			document.getElementById("header").innerHTML = num1 + "-" + num2;
+			compAnswer = num1 - num2;
+			break;
+		case 3:
+			num1 = Math.floor(Math.random() * 10) + 1;
+			num2 = Math.floor(Math.random() * 10) + 1;
+			document.getElementById("header").innerHTML = num1 + "x" + num2;
+			compAnswer = num1 * num2;
+			break;
+		case 4:
+			num1 = Math.floor(Math.random() * 10) + 1;       //divisor
+			num2 = (Math.floor(Math.random() * 10) + 1)*num1;  //devidend
+			document.getElementById("header").innerHTML = num2 + "/" + num1;
+			compAnswer = num2 / num1;
+			break;
+	}
+
 }
 
-setTimeout(function(){ 
-alert("Score :"+document.getElementById("output").innerHTML); }, 15000);
+/*setTimeout(function () {
+	alert("Score :" + document.getElementById("output").innerHTML);
+}, 15000);*/
 
 
 
-function SetFocus()
-{
-    // safety check, make sure its a post 1999 browser
-    if (!document.getElementById)
-    {
-        return;
-    }
+function SetFocus() {
+	// safety check, make sure its a post 1999 browser
+	if (!document.getElementById) {
+		return;
+	}
 
-    var txtMyInputBoxElement = document.getElementById("answer");
+	var txtMyInputBoxElement = document.getElementById("answer");
 
-    if (txtMyInputBoxElement != null)
-    {
-        txtMyInputBoxElement.focus();
-    }
+	if (txtMyInputBoxElement != null) {
+		txtMyInputBoxElement.focus();
+	}
 }
 
 //function move progress bar
 function move() {
-  var elem = document.getElementById("myBar");   
-  var width = 0;
-  var id = setInterval(frame, 100);
+	var elem = document.getElementById("myBar");
+	var width = 0;
+	var id = setInterval(frame, 100);
 
-  function frame() {
-    if (width == 100) {
-      clearInterval(id);
-	  alert("Score :"+document.getElementById("output").innerHTML);
-	  location.reload();
-    } else {
-      width++; 
-      elem.style.width = width + '%'; 
-    }
-  }
+	function frame() {
+		if (width == 100) {
+			clearInterval(id);
+			alert("Score :" + document.getElementById("output").innerHTML);
+			location.reload();
+		} else {
+			width++;
+			elem.style.width = width + '%';
+		}
+	}
 }
 
 
@@ -98,5 +112,5 @@ function move() {
 
 
 
-		
-	
+
+
